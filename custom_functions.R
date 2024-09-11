a5d865ab7d3b85d32f187f031dbc8db50a1d47a7
NataliaCiria <nataliaciria@hotmail.com>
2024-09-11 04:42:35 +0200

    Add model equations and create results summary and boxplots (based on custom functions)

---
+++ custom_functions.R
@@ -0,0 +1,106 @@
+#' Summarize Stochastic Node with Key Columns
+#'
+#' @param mcnode A Monte Carlo node object
+#' @param data A dataframe containing key columns
+#' @param keys Vector of column names to use as keys (default: NULL)
+#'
+#' @return A dataframe with stochastic node summary and key columns
+#'
+#' @importFrom stats summary
+#' @importFrom utils unlist
+mc_keys_summary <- function(mcnode, data, keys = NULL) {
+  # If keys is NULL, use factor columns from data as keys
+  if (is.null(keys)) {
+    keys <- names(data[sapply(data, is.factor)])
+  }
+  
+  # Access summary data frame (provided in a list)
+  summary_l <- summary(mcnode)[[1]]
+  
+  # If it's a single line mcnode, save single df summary in list for compatibility
+  if (!is.list(summary_l)) {
+    summary_l <- list(summary_l)
+  }
+  
+  # Extract column names
+  summary_names <- colnames(summary_l[[1]])
+  
+  # Summarize all mcnode variates in a dataframe
+  summary_df <- data.frame(matrix(unlist(summary_l), nrow = length(summary_l), byrow = TRUE))
+  
+  # Include column names
+  names(summary_df) <- paste(summary_names)
+  
+  # Merge keys and summary
+  summary_df <- cbind(data[keys], summary_df)
+  
+  return(summary_df)
+}
+
+#' Convert Monte Carlo Node to Long Format
+#'
+#' @param mcnode A Monte Carlo node object
+#' @param data A dataframe containing key columns
+#' @param keys Vector of column names to use as keys (default: NULL)
+#'
+#' @return A long format dataframe of the Monte Carlo node
+#'
+#' @importFrom tidyr pivot_longer unite
+#' @importFrom dplyr %>%
+long_mc <- function(mcnode, data, keys = NULL) {
+  # If keys is NULL, use factor columns from data as keys
+  if (is.null(keys)) {
+    keys <- names(data[sapply(data, is.factor)])
+  }
+  
+  # Use mean as central measure (for ordering etc.)
+  node_summary <- mc_keys_summary(mcnode, data)
+  central_col <- node_summary$mean
+  
+  # Convert mcnode to long data frame
+  mcnode_long <- as.data.frame(t(unmc(mcnode)))
+  
+  # Merge keys and summary
+  results <- cbind(data[keys], central_col, mcnode_long) %>%
+    pivot_longer(cols = starts_with("V", ignore.case = FALSE)) %>%
+    unite(key, all_of(keys), remove = FALSE)
+  
+  return(results)
+}
+
+#' Create Boxplot for Long Format Monte Carlo Data
+#'
+#' @param data A long format dataframe of Monte Carlo data
+#' @param value Column name for values (default: "value")
+#' @param key_label Column name for key labels (default: "key")
+#' @param central_col Column name for central measure (default: "central_col")
+#'
+#' @return A ggplot object representing the boxplot
+#'
+#' @importFrom ggplot2 ggplot aes geom_point geom_boxplot scale_color_gradient scale_fill_gradient scale_x_discrete theme_minimal coord_flip theme element_text
+#' @importFrom dplyr %>% group_by_at summarise
+long_mc_boxplot <- function(data, value = "value", key_label = "key", central_col = "central_col") {
+  my_labels <- data %>%
+    group_by_at(c("key", key_label)) %>%
+    summarise(central_col = mean(!!sym(value)), .groups = "keep")
+  
+  my_labels <- my_labels[order(my_labels$central_col), ][[key_label]]
+  
+  data %>%
+    ggplot(aes(x = reorder(key, !!sym(value)), y = !!sym(value), fill = !!sym(value), colour = !!sym(value))) +
+    geom_point(aes(x = reorder(key, !!sym(value)), y = !!sym(value)),
+               position = position_jitter(width = 0.15, height = 0),
+               size = 0.5, alpha = 0.2) +
+    geom_boxplot(alpha = 0.4, aes(colour = !!sym(central_col)), fill = "white", outlier.alpha = 0) +
+    scale_color_gradient(low = "#119da4", high = "#fe6847", guide = NULL) +
+    scale_fill_gradient(low = "#119da4", high = "#fe6847", guide = NULL) +
+    scale_x_discrete(labels = my_labels) +
+    theme_minimal() +
+    coord_flip() +
+    theme(
+      axis.text.x = element_text(angle = 45, hjust = 1),
+      axis.text.y = element_text(size = 12),
+      legend.position = "left",
+      plot.title = element_text(size = 12)
+    )
+}
--- model.R
+++ model.R
@@ -1,61 +1,105 @@
 #### Set-up ####
 
-#open risk_analysis_template.Rproj (RECOMMENDED)
-#or set working directory to this folder:
-#setwd("[directory]\\risk_analysis_template")
-#install.packages(c("mc2d","ggplot2"))
+# Open risk_analysis_template.Rproj (RECOMMENDED)
+# Or set working directory to this folder:
+# setwd("[directory]\\risk_analysis_template")
+# install.packages(c("mc2d", "ggplot2"))
 
-#Load packages
-library(mc2d) # Monte-Carlo simulations
-library(ggplot2) #Plot graphs
+# Load packages
+library(mc2d)    # Monte-Carlo simulations
+library(ggplot2) # Plot graphs
 
-#Load data
-data<-read.csv("data.csv", sep=";" ,stringsAsFactors=TRUE, na.strings=c(NA,"NA",""))
-print(data) #One row equals one variate in Monte Carlo simulation
+# Load data
+data <- read.csv(
+  "data.csv",
+  sep = ";",
+  stringsAsFactors = TRUE,
+  na.strings = c(NA, "NA", "")
+)
+print(data) # One row equals one variate in Monte Carlo simulation
 
-#### Create mc objects (mcnodes) #### 
+#### Create mc objects (mcnodes) ####
 
-##### Fixed variables ##### 
-mcnodes_name<-names(data[sapply(data,is.numeric)])
+##### Fixed variables #####
+mcnodes_name <- names(data[sapply(data, is.numeric)])
 
-for(i in 1:length(mcnodes_name)){
-  mcnode_i<-mcdata(data[[mcnodes_name[i]]],type="0",nvariates=nrow(data))
-  assign(mcnodes_name[i],mcnode_i)
+for (i in 1:length(mcnodes_name)) {
+  mcnode_i <- mcdata(data[[mcnodes_name[i]]], type = "0", nvariates = nrow(data))
+  assign(mcnodes_name[i], mcnode_i)
 }
 
-##### Stochastic Variables ##### 
-h_prev<-mcstoc(func=runif,
-               min=h_prev_min, 
-               max=h_prev_max, 
-               nvariates=nrow(data))
+##### Stochastic Variables #####
+h_prev <- mcstoc(
+  func = runif,
+  min = h_prev_min,
+  max = h_prev_max,
+  nvariates = nrow(data)
+)
 
-w_prev<-mcstoc(func=runif,
-               min=w_prev_min, 
-               max=w_prev_max, 
-               nvariates=nrow(data))
+w_prev <- mcstoc(
+  func = runif,
+  min = w_prev_min,
+  max = w_prev_max,
+  nvariates = nrow(data)
+)
 
-n_animals<-mcstoc(func=rnorm,
-                  mean=n_animals_mean,
-                  sd=n_animals_sd,
-                  nvariates=nrow(data))
+n_animals <- mcstoc(
+  func = rnorm,
+  mean = n_animals_mean,
+  sd = n_animals_sd,
+  nvariates = nrow(data)
+)
 
-test_sensi<-mcstoc(func=rpert,
-                   min=test_sensi_min, 
-                   mode=test_sensi_mode, 
-                   max=test_sensi_max, 
-                   nvariates=nrow(data))
-
+test_sensi <- mcstoc(
+  func = rpert,
+  min = test_sensi_min,
+  mode = test_sensi_mode,
+  max = test_sensi_max,
+  nvariates = nrow(data)
+)
 
 #### Write model ####
 
+# Probability that an animal in a herd is infected (a = an animal)
+a_inf <- h_prev * w_prev
 
+# Probability an animal is a false negative (test specificity assumed to be 100%)
+a_false_neg <- a_inf * (1 - test_sensi)
 
-#### Model output ####
+# Probability at least one animal from a farm is a false negative (aloa = at least one animal)
+aloa_false_neg <- 1 - (1 - a_false_neg) ^ n_animals
 
-##### Analyze #####
+# Probability at least one animal from a farm is a false negative (alof = at least one farm)
+alof_false_neg <- 1 - (1 - aloa_false_neg) ^ n_farms
 
-##### Plot #####
+# Results
+print(alof_false_neg)
 
-##### Save #####
+#### Further analysis ####
 
+# Load custom functions
+source("custom_functions.R")
 
+# Load packages
+library(dplyr)   # Data manipulation
+library(tidyr)   # Data cleaning
+library(ggplot2) # Plot graphs
+
+# Results mcnode
+result_mc <- alof_false_neg
+
+# Summarize adding data keys
+# If keys is null, defaults to factor columns
+mc_keys_summary(result_mc, data)
+
+# Transform mc to long object
+# This allows compatibility with most tidyverse functions (such as ggplot)
+result_long <- long_mc(result_mc, data)
+
+# Boxplot by key
+long_mc_boxplot(result_long)
+
+# Result is a ggplot, then you can use all ggplot functionalities
+# For example, wrap by disease
+long_mc_boxplot(result_long, key_label = "origin") +
+  facet_wrap(vars(disease), scales = "free")
